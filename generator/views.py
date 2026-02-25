39e82d609a51e2362ef20a51225358092b9c5b1f
Carolina <celycarolinagarciaalvarez.com>
2026-02-25 10:50:20 -0500

    Contraseñas

---
+++ generator/views.py
@@ -0,0 +1,8 @@
+import random
+import string
+from django.shortcuts import render
+
+def home(request):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(random.choice(caracteres) for _ in range(12))
+    return render(request, "generator/home.html", {"password": password})
